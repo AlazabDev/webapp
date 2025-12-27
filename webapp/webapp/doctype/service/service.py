e1585ec6ddc6ced220fed017628df5fe4fb3862c
Soumya Kavali <soumyakavali215@gmail.com>
2025-12-27 13:17:01 +0000

    Fixed ruff format issue.

--- webapp/webapp/doctype/service/service.py
+++ webapp/webapp/doctype/service/service.py
@@ -1,9 +1,10 @@
 import frappe
 from frappe.website.website_generator import WebsiteGenerator
 
+
 class Service(WebsiteGenerator):
-  website = frappe._dict(
-        template = "templates/generators/service.html",
-        condition_field = "published",
-        page_title_field = "service_name",
-    )
+	website = frappe._dict(
+		template="templates/generators/service.html",
+		condition_field="published",
+		page_title_field="service_name",
+	)

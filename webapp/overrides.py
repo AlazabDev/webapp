4c5e83448fe5e46c1a0e30f00cdb2f27d03e150d
Alazab Developer <mohamed@alazab.com>
2026-07-11 22:01:23 +0300

    fix shared website context

--- webapp/overrides.py
+++ webapp/overrides.py
@@ -2,14 +2,12 @@ import frappe
 
 
 def website_context(context):
-	context.services = frappe.get_all(
-		"Service",
-		filters={"published": 1},
-		fields=["name", "service_name", "short_description", "service_image", "route"],
-		order_by="creation asc",
-	)
-	context.all_blogs = frappe.get_all(
-		"Blog Post",		
-		order_by="creation asc",
-	)
-	return context
+    """Provide shared, read-only website navigation data."""
+    context.services = frappe.get_all(
+        "Service",
+        filters={"published": 1},
+        fields=["name", "service_name", "short_description", "service_image", "route", "icon"],
+        order_by="service_name asc",
+        limit_page_length=50,
+    )
+    return context
